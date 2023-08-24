2aabc735a62ccfa420415972a182291368de6dc6
ebiapollos <doyinapollos@gmail.com>
2023-08-24 13:31:51 +0100

    tablet-view

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //alert('hi')
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", () => {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
